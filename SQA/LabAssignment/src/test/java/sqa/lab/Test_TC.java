6082b8c5dc933bcba4659ea4f6cbfee5d47962d7
Phatcharapong <phatcharapong.s@kkumail.com>
2026-07-08 23:30:46 +0700

    Submit

--- SQA/LabAssignment/src/test/java/sqa/lab/Test_TC.java
+++ SQA/LabAssignment/src/test/java/sqa/lab/Test_TC.java
@@ -8,7 +8,7 @@ public class Test_TC {
     @Test
     public void testTC01() {
         ShiftCipher cipher = new ShiftCipher();
-        // คาดหวัง ZVMADHYL (ตามชีทอาจารย์)
+       
         String result = cipher.shift("SOFTWARE", 3);
         assertEquals("ZVMADHYL", result);
     }
@@ -16,7 +16,7 @@ public class Test_TC {
     @Test
     public void testTC02() {
         ShiftCipher cipher = new ShiftCipher();
-        // คาดหวัง zvmadhyl (ตามชีทอาจารย์)
+        
         String result = cipher.shift("software", 3);
         assertEquals("zvmadhyl", result);
     }
@@ -24,7 +24,7 @@ public class Test_TC {
     @Test
     public void testTC03() {
         ShiftCipher cipher = new ShiftCipher();
-        // คาดหวัง ORRNPRR (กรณีคิดเพิ่ม: พิมพ์ใหญ่)
+        
         String result = cipher.shift("LOOKMOO", 3);
         assertEquals("ORRNPRR", result);
     }
@@ -32,7 +32,7 @@ public class Test_TC {
     @Test
     public void testTC04() {
         ShiftCipher cipher = new ShiftCipher();
-        // คาดหวัง orrnprr (กรณีคิดเพิ่ม: พิมพ์เล็ก)
+      
         String result = cipher.shift("lookmoo", 3);
         assertEquals("orrnprr", result);
     }
